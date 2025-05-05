baadbca7baecf73f528b49cce3e2459417266b28
EmpexOri <Oliver.rayward@icloud.com>
2025-05-06 02:42:03 +1000

    Character & Tilemaps

---
+++ NotMadFather/Assets/Assets/Art/Tilesets/S Frisk - Midnight Lilac Tileset/README.md
@@ -0,0 +1,20 @@
+# Midnight Lilac by S Frisk
+
+a 32px moody interior tileset for when you want things cute, but maybe a little spooky
+
+Inspired by the [wisteric-17](https://lospec.com/palette-list/wisteric-17) by [yedamameday](https://lospec.com/yedamameday) palette on Lospec!.
+
+## Note: In Development!
+
+I have additional features I would like to add to this pack including:
+
+- Dining Room
+- Living Room
+- Bathroom
+- More decor!
+
+## License
+
+This asset pack can be used in any non-commercial project, you may modify the asset as you wish. This asset pack can't be used in any commercial project, resold/redistributed, even if modified
+
+Please Credit : S Frisk or use [this link](https://sarahfrisk.com/)
---
+++ NotMadFather/Assets/Assets/Scripts/TopDownController.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+
+public class TopDownController : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    private Vector2 movement;
+    private Vector2 lastMoveDir = Vector2.down;
+
+    [Header("Idle Sprites")]
+    public Sprite idleDown;
+    public Sprite idleUp;
+    public Sprite idleRight;
+
+    [Header("Walking Animations")]
+    public RuntimeAnimatorController walkAnimator; // Optional, used only when moving
+
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        animator = gameObject.AddComponent<Animator>();
+        animator.runtimeAnimatorController = null; // Ensure we start idle
+    }
+
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Store last movement direction
+        if (movement != Vector2.zero)
+        {
+            lastMoveDir = movement;
+
+            // Enable walk animations
+            if (walkAnimator != null && animator.runtimeAnimatorController != walkAnimator)
+                animator.runtimeAnimatorController = walkAnimator;
+
+            // Flip sprite for left
+            if (movement.x < 0)
+                spriteRenderer.flipX = true;
+            else if (movement.x > 0)
+                spriteRenderer.flipX = false;
+
+            if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+            {
+                animator.SetInteger("direction", 1); // Right or Left
+
+                if (movement.x < 0)
+                    spriteRenderer.flipX = true;
+                else
+                    spriteRenderer.flipX = false;
+            }
+            else if (movement.y > 0)
+            {
+                animator.SetInteger("direction", 2); // Up
+                spriteRenderer.flipX = false;
+            }
+            else if (movement.y < 0)
+            {
+                animator.SetInteger("direction", 0); // Down
+                spriteRenderer.flipX = false;
+            }
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+
+            // Remove animator to switch to static sprite
+            animator.runtimeAnimatorController = null;
+
+            // Select idle sprite based on last move direction
+            if (Mathf.Abs(lastMoveDir.x) > Mathf.Abs(lastMoveDir.y))
+            {
+                spriteRenderer.sprite = idleRight;
+                spriteRenderer.flipX = lastMoveDir.x < 0;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+                if (lastMoveDir.y > 0)
+                    spriteRenderer.sprite = idleUp;
+                else
+                    spriteRenderer.sprite = idleDown;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
